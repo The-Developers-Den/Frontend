4594dfe7820993a984c0c79296be878be5f17a0b
Pratham <prathamsinghal2003@gmail.com>
2023-04-13 23:12:58 +0530

    feat: add header

---
+++ components/Header.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "./Menu";
+import MenuMobile from "./MenuMobile";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0yarm");
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] border-b flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show} font-monument_reg`}
+    >
+      <div className="w-full max-w-[1280px] px-5 md:px-10 mx-auto h-[60px] flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/assets/logo.svg"
+            className="w-[40px] md:w-[60px]"
+            width="60"
+            height="60"
+            alt="logo"
+          />
+        </Link>
+        <Menu />
+        {mobileMenu && <MenuMobile setMobileMenu={setMobileMenu} />}
+        <div className="font-monument_reg">
+          <ConnectButton
+            chainStatus="icon"
+            showBalance={false}
+            accountStatus={{
+              smallScreen: "avatar",
+              largeScreen: "full",
+            }}
+          />
+
+          {/* Mobile icon start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        monument_reg: "Monuments Ext, sans-serif",
+        monument_bold: "Monuments Ext Bold, sans-serif",
+        otf: "OTF, sans-serif",
       },
     },
   },
   plugins: [],
-}
+};
