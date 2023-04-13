97938f35d1febc33674baaea65cdd0339b621edb
Pratham <prathamsinghal2003@gmail.com>
2023-04-13 21:39:16 +0530

    feat: Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,35 @@
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Roller Dao",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNCT_PROJECT_KEY,
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />;
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
