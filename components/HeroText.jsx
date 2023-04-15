daddcb9ea99976e3aca9760fb6653f156591f230
Pratham <prathamsinghal2003@gmail.com>
2023-04-15 11:59:12 +0530

    style: add card in top products

--- components/HeroText.jsx
+++ components/HeroText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroText = () => {
   return (
-    <div className="w-[50%] basis-[50%] justify-center text-center px-5  border-r font-otf  flex flex-col ">
+    <div className="w-[50%] basis-[50%] justify-center text-center px-5  md:border-r font-otf  flex flex-col ">
       <h1 className="text-7xl">Virtual</h1>
       <h1 className="text-7xl ">World</h1>
       <h2 className="text-[#A3A3A3] my-5 text-sm">
--- components/TopProducts.jsx
+++ components/TopProducts.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import TopProductsCard from "./TopProductsCard";
 
 const TopProducts = () => {
-  return <div className="min-h-[60vh] bg-[#7843E8]">TopProducts</div>;
+  return (
+    <div className="min-h-[60vh] bg-[#7843E8] grid md:grid-cols-2 gap-2 content-center max-md:grid-rows-2">
+      <TopProductsCard />
+      <TopProductsCard />
+    </div>
+  );
 };
 
 export default TopProducts;
